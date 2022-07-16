ce8b66d7841812db37fcd7e0cc48145200f96121
kalassak akani lapik <flerovium292@gmail.com>
2022-07-15 20:38:02 -0800

    updated code, definitely doesn't work with partially implemented advection

--- sst.py
+++ sst.py
@@ -3,6 +3,50 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 
+# dqdt requires a list of the time derivatives for q, stored 
+# in order from present to the past
+def ab_blend(dqdt,order):
+    if order==1:
+        return dqdt[0]
+    elif order==2:
+        return 1.5*dqdt[0]-.5*dqdt[1]
+    elif order==3:
+        return (23*dqdt[0]-16*dqdt[1]+5*dqdt[2])/12.
+    else:
+        print("order", order ," not supported ")
+
+def advect(u,b,dx,order):
+# Assumes u=0 at end points (a solid boundary). So no need to calculate
+# dbdx[0] and dbdx[-1]
+    dbdx=np.zeros(len(b))
+    
+    if order == 2: # 2nd-order centered scheme
+        dbdx[1:-1] =  (-b[0:-2] + b[2:])/(2*dx)
+
+    elif order == 1: # 1st order upwind
+        dbdx[1:-1] = np.where( u[1:-1]>0.,
+                              -b[0:-2] + b[1:-1] 
+                              ,        - b[1:-1] + b[2:] 
+                              )/dx
+        
+    elif order == 3: # 3rd order upwind. But 1st order one point from boundaries  
+        dbdx[1] = np.where( u[1]>0.,
+                              -b[0] + b[1] 
+                              ,     - b[1] + b[2] 
+                              )/dx
+        dbdx[-2] = np.where( u[-2]>0.,
+                              -b[-3] + b[-2] 
+                              ,      - b[-2] + b[-1] 
+                              )/dx
+        dbdx[2:-2] = np.where( u[2:-2]>0., 
+                +   b[:-4]  - 6*b[1:-3] + 3*b[2:-2] + 2*b[3:-1] ,
+                            - 2*b[1:-3] - 3*b[2:-2] + 6*b[3:-1] - b[4:] 
+                               )/(6*dx)
+    else:
+        print("no advection scheme for",order)
+        
+    return -u*dbdx
+
 def calc_dist(lat1,lon1,lat2,lon2):
 	R = 6371008.8
 	lat1 = np.radians([lat1])
@@ -17,14 +61,26 @@ def calc_dist(lat1,lon1,lat2,lon2):
 	d = R * c
 	return d
 
+#----------------------------
 RES_X = 64 #LONS
 RES_Y = 32 #LATS
-DEPTH = 300 #m
+DEPTH = 10 #m
 ITERS = 24*1000
 DT = 3600
 S_0 = 1371.0
-AXIS = 0*np.pi/180
+AXIS = 23.5*np.pi/180
 SB = 5.67*10**-8 #W m^-2 K^-4
+EMISSIVITY = 0.8
+R = 6371008.8 #m
+DAY = 86400 #s
+YEAR = 365*DAY
+RHO_0 = 1000. #kg/m^3
+f = 10**-4
+
+#equation of state
+T_0 = 283. #K
+ALPHA = 1.7*10**-4 #K^-1
+#----------------------------
 
 lon_bs = np.linspace(0, 360, RES_X+1)
 lat_bs = np.linspace(90, -90, RES_Y+1)
@@ -47,7 +103,7 @@ print ew_dists
 trpz_areas = ns_dist*(ew_dists[1:] + ew_dists[:-1])/2
 
 areas = np.rot90(np.tile(trpz_areas, 64).reshape((RES_Y, RES_X)))
-masses = 1000*DEPTH*areas
+masses = RHO_0*DEPTH*areas
 
 print areas
 
@@ -60,20 +116,75 @@ lons = lons*np.pi/180
 
 #initialize sst
 ssts = np.zeros((RES_X, RES_Y))+288.
+#initial atmospheric temperature
+atm_temp = 255. #K
+#spring equinox
+phi = 0
 
 mean_ssts = []
+atm_temps = []
+us = 0.*ssts
+vs = 0.*ssts
+dudt = 0.*us
+dvdt = 0.*vs
+dTdt = 0.*ssts
+dudta = [0.,0.,0.] # items will be arrays of dudt for Adams-Bashforth
+dvdta = [0.,0.,0.] # for v...
+dTdta = [0.,0.,0.] # for T...
+advord = 3
+aborder = 3
 for x in xrange(0, ITERS):
-	phi = 0 #year angle
-	#calculate radiation
-	E_sol_p_sqm = S_0/2 * (np.sin(lats)*np.sin(AXIS*np.sin(phi))+np.cos(lats)*np.cos(AXIS*np.sin(phi))) * DT
+	phi += 2*np.pi/YEAR * DT #year angle
+	#CALCULATE RADIATION
+	#sfc in (insolation)
+	E_sol_p_sqm = 0.70*S_0/np.pi * (np.sin(lats)*np.sin(AXIS*np.sin(phi))+np.cos(lats)*np.cos(AXIS*np.sin(phi))) * DT
 	E_sol_p_sqm = np.where(E_sol_p_sqm > 0., E_sol_p_sqm, 0.)
 	ssts += E_sol_p_sqm*areas/(masses*4184.)
 	
-	E_out_p_sqm = SB*ssts**4 * DT
-	ssts -= E_out_p_sqm*areas/(masses*4184.)
+	#sfc out (to space and atm)
+	E_sfcout_p_sqm = SB*ssts**4 * DT
+	ssts -= E_sfcout_p_sqm*areas/(masses*4184.)
 
-	print np.mean(ssts)
-	mean_ssts.append(np.mean(ssts))
+	#atmosphere layer
+	#simply solve for the temperature of the slab atmosphere
+	sfc_temp = np.sum(ssts*areas)/np.sum(areas) #area-weighted mean surface temperature
+	atm_temp = 255 #(0.5*sfc_temp**4)**0.25
+	
+	#sfc in (atmosphere)
+	E_atm2sfc_p_sqm = EMISSIVITY*SB*atm_temp**4 * DT
+	ssts += E_atm2sfc_p_sqm*areas/(masses*4184.)
+
+	print sfc_temp
+	mean_ssts.append(sfc_temp)
+	atm_temps.append(atm_temp)
+
+	#CALCULATE DENSITY
+	rhos = RHO_0*(1-ALPHA*(ssts-T_0))
+
+	#MOMENTUM EQUATIONS
+	dudt = f*vs #-1/RHO_0*dpdx
+	dvdt = - f*us #-1/RHO_0*dpdy
+	
+	#CALCULATE ADVECTION
+	dudt += advect(us,us,ew_dists,advord) #nonlinear advection term
+	dvdt += advect(vs,vs,ns_dist,advord) #nonlinear advection term
+
+	dTdt += advect(us,ssts,ew_dists,advord)
+	dTdt += advect(vs,ssts,ns_dist,advord)
+
+	# Adams-Bashforth time-step:
+	abnow = min(x,aborder)
+	dudta = [dudt.copy()] + dudta[:-1]
+	dvdta = [dvdt.copy()] + dvdta[:-1]
+	dTdta = [dTdt.copy()] + dTdta[:-1]
+	us += DT*ab_blend(dudta,abnow)
+	vs += DT*ab_blend(dvdta,abnow)
+	ssts += DT*ab_blend(dTdta,abnow)
+
+	#advect() only acts on one dimension
+	#fix ew_dists so advect() only gets one value
+	#fix u, v grid sizes
+	#make f a proper grid
 
 	#plot
 	if x % 2400 == 0:
@@ -104,6 +215,7 @@ fig = plt.figure(figsize=(18.6, 10.5))
 ax = fig.add_axes((0,0,1,1))
 
 plt.plot(np.arange(0, 24000, 1), mean_ssts, 'b-')
+plt.plot(np.arange(0, 24000, 1), atm_temps, 'r-')
 
 plt.savefig("/home/kalassak/sst/diag_mean_sst.png", bbox_inches='tight', pad_inches=0, dpi=100)
 plt.close()
@@ -115,4 +227,5 @@ plt.close()
 #- advection scheme
 #- wind stress (from plasim)
 #- write monthly means to .sra format
+#- water should freeze
 
